a7a983df3258ff77463fa900e3119f94e88764af
ebssgd <iamebssgd@yahoo.com>
2021-07-26 20:26:09 -0500

    Format is complete

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const http = require('http');
-const { resolve } = require('path');
+const http = require("http");
 const port = 3000;
+const handlers = require("./handlers");
 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-
-    res.write('Hello JS World!');
-    res.end();
-}).listen(port);
+http
+  .createServer((req, res) => {
+    for (let handler of handlers) {
+      if (!handler(req, res)) {
+        break;
+      }
+    }
+  })
+  .listen(port);
