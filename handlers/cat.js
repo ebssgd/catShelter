d826d8499dc0be1f4871a142b46e80d52221e162
ebssgd <iamebssgd@yahoo.com>
2021-07-26 21:38:38 -0500

    Need to finish GET requests and start POST requests

--- handlers/cat.js
+++ handlers/cat.js
@@ -5,3 +5,45 @@ const qs = require("querystring");
 const formidable = require("formidable");
 const breeds = require("../data/breeds");
 const cats = require("../data/cats");
+
+module.exports = (req, res) => {
+  const pathname = url.parse(req.url).pathname;
+
+  if (pathname === "/cats/add-cat" && req.method === "GET") {
+    let filepath = path.normalize(path.join(__dirname, "../views/addCat.html"));
+
+    const index = fs.createReadStream(filepath);
+
+    index.on("data", (data) => {
+      res.write(data);
+    });
+
+    index.on("end", () => {
+      res.end();
+    });
+
+    index.on("error", (err) => {
+      console.log(err);
+    });
+  } else if (pathname === "/cats/add-breed" && req.method === "GET") {
+    let filepath = path.normalize(
+      path.join(__dirname, "../views/addBreed.html")
+    );
+
+    const index = fs.createReadStream(filepath);
+
+    index.on("data", (data) => {
+      res.write(data);
+    });
+
+    index.on("end", () => {
+      res.end();
+    });
+
+    index.on("error", (err) => {
+      console.log(err);
+    });
+  } else {
+    return true;
+  }
+};
